5fa008d00ab45eea2b30bffb6213ace10320a48f
Gabriel <g.andrade@salu.com.vc>
2024-06-25 22:50:44 -0300

    feat: adjust code base

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -1,7 +1,8 @@
 package models
 
 type User struct {
-	ID       uint64 `gorm:"primary_key;auto_increment"`
-	Username string `gorm:"type:varchar(100);not null"`
-	Email    string `gorm:"type:varchar(100);unique_index;not null"`
+	ID       uint64 `gorm:"primary_key;auto_increment" json:"id"`
+	Name     string `gorm:"type:varchar(100);not null" json:"name"`
+	Username string `gorm:"type:varchar(100);not null" json:"username"`
+	Email    string `gorm:"type:varchar(100);unique_index;not null" json:"email"`
 }
