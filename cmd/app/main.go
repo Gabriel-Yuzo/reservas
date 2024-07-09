7593ac5fac6064907f1462cbda9cc223df821db3
Gabriel-Yuzo <gandrade529@gmail.com>
2024-07-09 17:50:05 -0300

    fix project

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/Gabriel-Yuzo/reservas/docs"
 	ginHttp "github.com/Gabriel-Yuzo/reservas/internal/api"
 	"github.com/Gabriel-Yuzo/reservas/internal/config"
 	"github.com/Gabriel-Yuzo/reservas/internal/domain/models"
 	database "github.com/Gabriel-Yuzo/reservas/internal/infra/adpters/mysql"
 	"github.com/Gabriel-Yuzo/reservas/internal/repository"
 	"github.com/Gabriel-Yuzo/reservas/internal/usecase"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // @title Swagger Example API
@@ -40,9 +37,6 @@ func main() {
 	userRepo := repository.NewUserRepositoryImpl(db)
 	userUsecase := usecase.NewUserUsecaseImpl(userRepo)
 	router := ginHttp.NewRouter(userUsecase)
-	// Documentação Swagger
-	docs.SwaggerInfo.BasePath = "/api"
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	// Iniciar o servidor
 	if err := router.Run(":" + cfg.Port); err != nil {
